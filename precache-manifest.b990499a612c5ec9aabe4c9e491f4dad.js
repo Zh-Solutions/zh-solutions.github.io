6480af2814fd0e9a5e1d73a788afa53e6d2aad49
Stefan Zhelyazkov <stefan.zhelyazkov@gmail.com>
2020-04-28 15:01:08 +0300

    Updates

---
+++ precache-manifest.b990499a612c5ec9aabe4c9e491f4dad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b661100a6880afb6ece02293601bd1ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a997e4c3540695c279e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "816add12b41a1945bcd8",
+    "url": "/static/js/2.795a60f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.795a60f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a997e4c3540695c279e",
+    "url": "/static/js/main.ca404217.chunk.js"
+  },
+  {
+    "revision": "1081d7a8252e02a2bb03",
+    "url": "/static/js/runtime-main.546e762e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
